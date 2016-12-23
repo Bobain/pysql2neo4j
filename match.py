faee6f8936b6e9971b5fefb116266be2a3740ecc
root <root@ns3017317.ip-149-202-79.eu>
2016-12-23 14:29:38 +0100

    creating backed_projects_match from 10 users to all others related takes 180 seconds so the whole computations would take 200 days... something is wrong here... we need to drop a lot of things before trying cagain

--- match.py
+++ match.py
@@ -19,11 +19,12 @@ if __name__ == '__main__':
     req = """
             MATCH
                     (u1:Users)-[r1:BACKED]->(p1:Projects)<-[r2:BACKED]-(u2:Users)
+            USING INDEX u2:Users(nb_projects_contributed)
+            WHERE ID(u1) < ID(u2) AND ID(u1) >= %d and ID(u1) < %d and u1.nb_projects_contributed > 1 and u2.nb_projects_contributed > 1
             WITH u1, u2, count(p1) as nb_common_projects
-            WHERE ID(u1) < ID(u2) AND ID(u1) >= %d and ID(u1) < %d
             CREATE UNIQUE (u1)-[r:BACKED_PROJECTS_MATCH {nb_projects: nb_common_projects}]-(u2)
         """
-    n = 1000
+    n = 10
     for i in xrange(0, 1500000, n):
         s = req % (i, i+n)
         print("Running cypher statement : \n\t%s" % s)
