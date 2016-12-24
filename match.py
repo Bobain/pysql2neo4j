fbf5497442ff7f7724c8dc7d68310b726acf20ed
Bobain <romain.burgot@gmail.com>
2016-12-24 14:09:52 +0100

    tons of errors in cypher requests: need to come back to local computer to write this correctly

--- match.py
+++ match.py
@@ -18,16 +18,20 @@ if __name__ == '__main__':
     run_cyper_script(graph, './match_scripts/cypher_scripts/match.cql')
 
     req = """
-            MATCH
+            MATCH 
                     (u1:Users)-[r1:BACKED]->(p1:Projects)<-[r2:BACKED]-(u2:Users)
-            WHERE ID(u1) < ID(u2) AND ID(u1) >= %d and ID(u1) < %d
+            WHERE ID(u1) = %d and %d < ID(u2)
             WITH u1, u2, count(p1) as nb_common_projects
             CREATE UNIQUE (u1)-[r:BACKED_PROJECTS_MATCH {nb_projects: nb_common_projects}]-(u2)
         """
-    n = 10
-    for i in xrange(0, graph.graphDb.evaluate('MATCH (u:Users) RETURN COUNT(*)'), n):
-        s = req % (i, i+n)
-        print("Running cypher statement : \n\t%s" % s)
-        start = time.time()
+    n = graph.graphDb.evaluate('MATCH (u:Users) RETURN COUNT(*)')
+    print('Running a query to create relationships for all %d users' % n)
+    n_chunk_print = 10000
+    for i in xrange(1, n+1):
+        s = req % (i, i)
+        if i % n_chunk_print == 1:
+            print("Running cypher statement : \n\t%s" % s)
+            start = time.time()
         graph.cypher_exec(s)
-        print("\n\t it took: %g " % (time.time() - start))
+        if i % n_chunk_print == 0:
+            print("\n\t it took: %g " % (time.time() - start))
